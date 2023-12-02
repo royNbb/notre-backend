b0a8e2a610bedcb3ed2e7cccf4b26ebeb4973b45
FreeJ1nG <Andrewjeremy12345@gmail.com>
2023-12-02 16:43:55 +0700

    feat: add documentation for create_history

--- jawabanku/history/services.py
+++ jawabanku/history/services.py
@@ -16,6 +16,17 @@ class HistoryService:
     return self.history_accessor.get_history_of_account(account)
 
   def create_history(self, account: Account, **kwargs) -> Optional['UserHistory']:
+    """Create History for a User
+    Pass along a dictionary containing, related_model_app_label, related_model_name, related_model_id
+    owner_id will be infered from account, so just pass along request.user
+    Example Usage:
+    data = {
+      "related_model_app_label": "comment",
+      "related_model_name": "comment",
+      "related_model_id": 2, // this specifies the comment id which we are making the history of
+    }
+    create_history(request.user, **data)
+    """
     parsed_data = {}
     parsed_data['owner_id'] = account.id
     if kwargs.get('related_model_app_label', None):
