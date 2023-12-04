69d304f27acd6bdb45e1590b131aee5165d0ac1a
FreeJ1nG <Andrewjeremy12345@gmail.com>
2023-12-04 20:58:21 +0700

    feat: add material to allowed_related_field for report

--- jawabanku/report/models.py
+++ jawabanku/report/models.py
@@ -1,4 +1,3 @@
-from tkinter import CASCADE
 from django.db import models
 from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericForeignKey
@@ -8,29 +7,26 @@ from account.models import Account
 
 
 class Report(models.Model):
-    reporter = models.ForeignKey(
-        Account, on_delete=models.CASCADE, blank=True, null=True
-    )
-    description = models.TextField()
-    status = models.BooleanField(default=False)
-    allowed_related_models = models.Q(app_label="comment", model="comment")
-    content_type = models.ForeignKey(
-        ContentType,
-        on_delete=models.CASCADE,
-        limit_choices_to=allowed_related_models,
-        related_name="report_type",
-    )
-    object_id = models.PositiveIntegerField()
-    report_of = GenericForeignKey(
-        ct_field="content_type",
-        fk_field="object_id",
-    )
+  description = models.TextField()
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+  allowed_related_models = models.Q(app_label='comment', model='comment') | models.Q(app_label='material', model='material')
+  content_type = models.ForeignKey(
+      ContentType,
+      on_delete=models.CASCADE,
+      limit_choices_to=allowed_related_models,
+      related_name='report_type',
+  )
+  object_id = models.PositiveIntegerField()
+  report_of = GenericForeignKey(
+      ct_field='content_type',
+      fk_field='object_id',
+  )
 
-    class Meta:
-        indexes = [models.Index(fields=["content_type", "object_id"])]
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
-        return f"Report(report_of={self.report_of}, description={self.description})"
+  class Meta:
+    indexes = [models.Index(fields=["content_type", "object_id"])]
+
+  def __str__(self) -> str:
+    return f"Report(report_of={self.report_of}, description={self.description})"
